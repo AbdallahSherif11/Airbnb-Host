ff9594c19226288a672e928d034aaf718e3f1c33
Ahmed Atef <ahmed.shera10@gmail.com>
2025-04-11 18:29:39 +0200

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { FlowbiteService } from './shared/services/flowbite.service';
+import { NavbarComponent } from "./core/layout/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
