4684fdaeb493543b008cbc42779cf3a30c0a6264
Abdallah Sherif <abdallahsherif_2000@yahoo.com>
2025-04-15 20:55:28 +0200

    combine booking and date picker edits

--- src/app/features/pages/house-details/house-details.component.ts
+++ src/app/features/pages/house-details/house-details.component.ts
@@ -9,11 +9,12 @@ import { AuthLayoutComponent } from "../../../core/layout/auth-layout/auth-layou
 import { BookingComponent } from "../../components/booking/booking.component";
 import { AmenitiesComponent } from "../../components/amenities/amenities.component";
 import { HouseMapComponent } from '../../components/house-map/house-map.component';
+import { DateRangePickerComponent } from '../../components/date-range-picker/date-range-picker.component';
 
 @Component({
   selector: 'app-house-details',
   standalone: true,
-  imports: [CommonModule, HouseImagesComponent, NavbarComponent, FooterComponent, AuthLayoutComponent, BookingComponent, HouseMapComponent],
+  imports: [CommonModule, HouseImagesComponent, NavbarComponent, FooterComponent, AuthLayoutComponent, BookingComponent, HouseMapComponent, DateRangePickerComponent],
   templateUrl: './house-details.component.html',
   styleUrls: ['./house-details.component.css']
 })
@@ -27,6 +28,13 @@ export class HouseDetailsComponent {
   house: any;
   isLoading = true;
   error: string | null = null;
+  checkInDate: Date | null = null;
+  checkOutDate: Date | null = null;
+
+  onDatesSelected(event: {checkIn: Date | null, checkOut: Date | null}): void {
+    this.checkInDate = event.checkIn;
+    this.checkOutDate = event.checkOut;
+}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
