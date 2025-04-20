097b0edec68bf46456da4b29746e966c07434f04
Abdallah Sherif <abdallahsherif_2000@yahoo.com>
2025-04-20 08:15:25 +0200

    Wishlist Require Login

--- src/app/features/pages/wishlist/wishlist.component.ts
+++ src/app/features/pages/wishlist/wishlist.component.ts
@@ -5,18 +5,19 @@ import { HouseCardComponent } from '../../components/house-card/house-card.compo
 import { NavbarComponent } from "../../../core/layout/navbar/navbar.component";
 import { FooterComponent } from "../../../core/layout/footer/footer.component";
 import { FormsModule } from '@angular/forms';
-
 import { RouterLink } from '@angular/router';
+import { AccountService } from '../../../core/services/account/account.service';
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [CommonModule, HouseCardComponent, NavbarComponent, FooterComponent,FormsModule,RouterLink],
+  imports: [CommonModule, HouseCardComponent, NavbarComponent, FooterComponent, FormsModule, RouterLink],
   templateUrl: './wishlist.component.html',
   styleUrls: ['./wishlist.component.css']
 })
 export class WishlistComponent {
   private wishlistService = inject(WishlistService);
+  private accountService = inject(AccountService);
   searchQuery: string = '';
   wishlist$ = this.wishlistService.wishlist$;
   allHouseData: any[] = [];
@@ -24,28 +25,29 @@ export class WishlistComponent {
   router: any;
 
   constructor() {
-    this.wishlistService.getMyWishlist().subscribe({
-      next: (data) => {
-        this.allHouseData = data;
-        console.log('📦 Wishlist house data:', this.allHouseData);
-        this.isLoading = false;
-      },
-      error: (err) => {
-        console.error('❌ Error loading wishlist data:', err);
-        this.isLoading = false;
-      }
-    });
+    if (this.accountService.isLoggedIn()) {
+      this.wishlistService.getMyWishlist().subscribe({
+        next: (data) => {
+          this.allHouseData = data;
+        //   console.log('📦 Wishlist house data:', this.allHouseData);
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('❌ Error loading wishlist data:', err);
+          this.isLoading = false;
+        }
+      });
+    } else {
+      console.warn('User is not logged in. Skipping wishlist fetch.');
+      this.isLoading = false;
+    }
   }
 
-
-
-  
-
-onSearch(): void {
-  if (this.searchQuery.trim()) {
-    this.router.navigate(['/search'], {
-      queryParams: { q: this.searchQuery }
-    });
+  onSearch(): void {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/search'], {
+        queryParams: { q: this.searchQuery }
+      });
+    }
   }
-}
 }
